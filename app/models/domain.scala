d6a86b86cbe9de4d075f39d38b1440e3e2075d5b
Aina RAZAFIMAHEFA <nasoloaina@gmail.com>
2014-06-15 23:52:09 +0200

    Implemented SQL query results parsing with pattern matching

--- app/models/domain.scala
+++ app/models/domain.scala
@@ -27,4 +27,15 @@ object Product {
     }.toList
   }
 
+  /**
+   * Querying in Anorm with pattern matching
+   * @return
+   */
+  def getAllWithPatterns: List[Product] = DB.withConnection { implicit connection =>
+    import anorm.Row
+    sql().collect {
+      case Row(Some(id: Long), Some(ean: Long), Some(name: String), Some(description: String)) => Product(id, ean, name, description)
+    }.toList
+  }
+
 }
