a9fc8a4347b3ce90a6d876135465c840355837c6
Aina RAZAFIMAHEFA <nasoloaina@gmail.com>
2014-06-15 19:16:28 +0200

    Implemented SQL query results parsing with the Streaming API

---
+++ app/models/domain.scala
@@ -0,0 +1,26 @@
+package models
+
+import anorm.SqlQuery
+import anorm.SQL
+import play.api.db.DB
+
+/**
+ * Created by kaffein on 15/06/14.
+ */
+case class Product(id: Long, ean: Long, name: String, description: String)
+
+case class Warehouse(id: Long, name: String)
+
+case class StockItem(id: Long, productId: Long, warehouseId: Long, quantity: Long)
+
+object Product {
+
+  val sql: SqlQuery = SQL("select * from product order by name asc")
+
+  def getAll: List[Product] = DB.withConnection { implicit connection =>
+    sql().map { row =>
+      Product(row[Long]("id"), row[String]("name"), row[String]("description"))
+    }.toList
+  }
+
+}
